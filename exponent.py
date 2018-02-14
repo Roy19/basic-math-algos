02464fd717d98a765530d2a1fe7868beaf8262df
Aritra Sur Roy <aritra1997@hotmail.com>
2018-02-14 09:54:05 +0530

    Add Modular Exponent

--- exponent.py
+++ exponent.py
@@ -7,9 +7,19 @@ def exponent(num,pow):
 		return exponent(num,pow//2)**2
 	else:
 		return num*(exponent(num,(pow-1)//2)**2)
+
+def modularExponent(num,pow,M=(10**9)+7):
+    if pow == 0:
+        return 1
+    elif pow == 1:
+        return num % M
+    elif pow % 2 == 0:
+        return (modularExponent(num,pow//2,M)**2) % M
+    else:
+        return (num*(modularExponent(num,pow//2,M)**2)) % M
 		
 if __name__ == '__main__':
 	n,p = input().strip().split(' ')
 	n,p = [int(n),int(p)]
-	print("n^p = %d"%(exponent(n,p)))
+	print("n^p = %d"%(modularExponent(n,p)))
 	
